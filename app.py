2f77005905dc5b72c3b78557bc8246af5d8e0a0b
Miłosz Malak <malakmilosz@gmail.com>
2022-01-20 14:01:54 +0100

    Added response when user did not specify parameter

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
-from flask import Flask
+from flask import Flask,request
 app = Flask(__name__)
+
+@app.route("/losowa_liczba")
+def parameter():
+ type = request.args.get('typ')
+ if(type==None or type==''):  # Parameter is not specified
+     return f'Podanie typu jest wymagane.'
 
 
 if __name__ == '__main__':
