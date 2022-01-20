10bd7c74af532da7914cda9b4f8ea43f8c033433
Miłosz Malak <malakmilosz@gmail.com>
2022-01-20 22:08:29 +0100

    Re-coded for PEP8 requirements, added requirements.txt

--- app.py
+++ app.py
@@ -1,19 +1,26 @@
-from flask import Flask,request
+from flask import Flask, request
 import random
 app = Flask(__name__)
 
+
 @app.route("/losowa_liczba")
 def parameter():
- type = request.args.get('typ')
- if(type==None or type==''):  # Parameter is not specified
-     return f'Podanie typu jest wymagane.'
- if(type!='parzyste' and type!='nieparzyste'):  # The specified parameter is incorrect
-     return f'Parametr "typ" musi mieć wartość "parzyste" lub "nieparzyste".'
- else:
-       if type == 'parzyste':
-           return str(random.randrange(0,101,2))  # Generates a pseudo-random even number
-       else:
-           return str(random.randrange(1,100,2))  # Generates a pseudo-random odd number
+    type = request.args.get('typ')
+
+    # Parameter is not specified
+    if(type is None or type == ''):
+        return f'Podanie typu jest wymagane.'
+
+    # The specified parameter is incorrect
+    if(type != 'parzyste' and type != 'nieparzyste'):
+        return f'Parametr "typ" musi mieć wartość \
+        "parzyste" lub "nieparzyste".'
+    else:
+        if type == 'parzyste':
+            return str(random.randrange(0, 101, 2))
+        else:
+            return str(random.randrange(1, 100, 2))
+
 
 if __name__ == '__main__':
     app.run(debug=False)
