2e155c21aa702076a8a4403f91bd5aa0544ca10c
Miłosz Malak <malakmilosz@gmail.com>
2022-01-20 14:14:52 +0100

    Added case for "odd" and "even" parameter

--- app.py
+++ app.py
@@ -6,6 +6,14 @@ def parameter():
  type = request.args.get('typ')
  if(type==None or type==''):  # Parameter is not specified
      return f'Podanie typu jest wymagane.'
+ if(type!='parzyste' and type!='nieparzyste'):  # The specified parameter is incorrect
+     return f'Podany typ jest niepoprawny.'
+ else:
+       if type is 'parzyste':
+           return type # Code goes here
+       else:
+           return type # Code goes here
+    
 
 
 if __name__ == '__main__':
